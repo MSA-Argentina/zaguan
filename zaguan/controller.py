ad9a6ea3e71a46d144284b08a76d0ad36c666908
Felipe Lerena <felipelerena@gmail.com>
2013-02-13 13:30:25 -0300

    adding url processor generator

--- zaguan/controller.py
+++ zaguan/controller.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from json import dumps as to_json
+import urllib
+
+from json import dumps, loads
 
 from zaguan.container import launch_browser
 
@@ -27,20 +29,41 @@ class WebContainerController(object):
     def enviar_comando(self, comando, data=None):
         """Genera el comando Javascript a enviar a la interfaz web y lo envia.
         """
-        json_data = to_json(data).replace("\\\"", "\\\'")
+        json_data = dumps(data).replace("\\\"", "\\\'")
         self.send_function("run_op('%s', '%s')" % (comando, json_data))
 
-    def get_browser(self, uri):
+    def get_browser(self, uri, debug=False):
         """Obtiene el objeto browser y lo prepara para poder ser usada en este
         contexto.
 
         Devuelve el objeto browser y setea la funcion de envio para la clase.
         """
-        browser, web_send = launch_browser(uri, echo=False)
+        browser, web_send = launch_browser(uri, echo=debug)
         self.send_function = web_send
 
         browser.connect("resource-request-starting",
                         self.on_navigation_requested)
         return browser
 
+    def add_processor(self, url_word, callback):
+        def _inner(uri):
+            scheme, path = uri.split(':', 1)
+            if scheme == "http":
+                parts = path.split("/")[2:]
+                if parts[0] == url_word:
+                    remain = parts[1]
+                elif parts[1]  == url_word:
+                    remain = parts[2]
+                else:
+                    remain = None
+                if remain is not None:
+                    try:
+                        action, data = remain.split("?")
+                    except ValueError:
+                        action = remain
+                        data = "null"
 
+                    data = loads(urllib.unquote(data))
+                    return callback(action, data)
+
+        self.processors.append(_inner)

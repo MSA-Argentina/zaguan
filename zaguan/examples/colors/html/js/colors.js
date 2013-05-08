c0858c2e49912bc269c6d8e6a068684a464e07e3
Florencia Mincucci <fmincucci@msa.com.ar>
2013-05-08 12:15:29 -0300

    Added colors example

--- zaguan/examples/colors/html/js/colors.js
+++ zaguan/examples/colors/html/js/colors.js
@@ -16,4 +16,18 @@ function get_url(action, data){
     return url;
 }
 
-$(document).ready(load_ready_msg);
+function change_color(color){
+    $("body").css("background-color",color);
+    $(".news").text("Background color changed to " + color );
+}
+
+function select_color(color){
+    send('select_color',color);
+}
+
+$(document).ready(function(){
+    load_ready_msg;
+    $(".color1").click(function() { send('select_color','red'); });
+    $(".color2").click(function() { send('select_color','green'); });
+    $(".color3").click(function() { send('select_color','blue'); });
+});
